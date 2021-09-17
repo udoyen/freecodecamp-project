ea2a63e7f1946affc12d9da9f776a26ca9fe254f
udoyen <datameshprojects@gmail.com>
2021-09-17 22:57:09 +0100

    All tests passed

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -100,7 +100,7 @@ def sanity_check(my_list: list) -> bool:
     return False
 
 
-def calculate_the_result(front_operand: int, back_operand: int, operator: str) -> int:
+def calculate_the_result(front_operand: int, back_operand: int, operator: str) -> str:
     """calculate_the_result.
 
     :param front_operand:
@@ -109,11 +109,11 @@ def calculate_the_result(front_operand: int, back_operand: int, operator: str) -
     :type back_operand: int
     :param operator:
     :type operator: str
-    :rtype: int
+    :rtype: str
     """
     if operator == '+':
-        return front_operand + back_operand
-    return front_operand - back_operand
+        return str(front_operand + back_operand)
+    return str(front_operand - back_operand)
 
 
 def extract_digits_helper(item: str) -> list:
@@ -125,42 +125,42 @@ def extract_digits_helper(item: str) -> list:
     """
     result = []
     if '+' in item:
-        front_operand = item[0:item.index('+') + 1]
-        back_operand = item[item.index('+'):]
-        for j in front_operand:
-            if not j.isdigit():
-                result.append(int(front_operand[0:front_operand.index(j)]))
-                break
-        for k in back_operand:
-            if k.isdigit():
-                result.append(int(back_operand[back_operand.index(k):]))
-                break
-        # get the final value and
+        front_operand = item.split()[0]
+        result.append(front_operand)
+
+        back_operand = item.split()[2]
+        result.append(back_operand)
+
+        operator = item.split()[1]
+
         # create a tuple from the result list
-        result.append(calculate_the_result(result[0], result[1], '+'))
+        result.append(calculate_the_result(int(front_operand), int(back_operand), operator))
         # add the operator to the result list
-        result.append('+')
+        result.append(operator)
 
     else:
-        front_operand = item[0:item.index('-') + 1]
-        back_operand = item[item.index('-'):]
-        for j in front_operand:
-            if not j.isdigit():
-                result.append(int(front_operand[0:front_operand.index(j)]))
-                break
-        for k in back_operand:
-            if k.isdigit():
-                result.append(int(back_operand[back_operand.index(k):]))
-                break
-        # get the final value and
+        front_operand = item.split()[0]
+        result.append(front_operand)
+
+        back_operand = item.split()[2]
+        result.append(back_operand)
+
+        operator = item.split()[1]
+
         # create a tuple from the result list
-        result.append(calculate_the_result(result[0], result[1], '-'))
+        result.append(calculate_the_result(int(front_operand), int(back_operand), operator))
         # add the operator to the result list
-        result.append('-')
+        result.append(operator)
     return result
 
 
-def extract_digits(my_ls: list):
+def extract_digits(my_ls: list) -> list:
+    """
+    Used to extract the problem digits
+    and create a list of tuples
+    :param my_ls:
+    :return: list
+    """
     problem_solutions = []
 
     for i in my_ls:
@@ -198,19 +198,43 @@ def calculate_line(result: dict, master_key: int, front_operand_key: int, back_o
         return " "
 
 
-def set_front_operand(result: dict, master_key: int, child_key: int) -> any:
+def set_front_operand_helper(result: dict, master_key: int) -> str:
+    """
+    Returns the left operand value with
+    the right space padding added
+    :param result:
+    :type result: dict
+    :param master_key:
+    :type master_key: int
+    :return: str
+    """
+    right_to_left_operand_mapping = {
+        4: {4: 2, 3: 3, 2: 4, 1: 5},
+        3: {3: 1, 2: 3, 1: 4},
+        2: {1: 3, 2: 1},
+        1: {1: 2}
+    }
+    # check which is the bigger of the two operands
+    # print(f"Master key 1: {len(str(result[master_key][1]))}")
+    if len(str(result[master_key][1])) >= len(str(result[master_key][2])):
+        # return the space padded string
+        return '''{0:>{1}}'''.format(result[master_key][1], len(str(result[master_key][1])) + 2)
+    else:
+        padding = right_to_left_operand_mapping[len(str(result[master_key][2]))][len(str(result[master_key][1]))]
+        return '''{0:>{1}}'''.format(result[master_key][1], padding + len(str(result[master_key][1])))
+
+
+def set_front_operand(result: dict, master_key: int) -> any:
     """set_front_operand.
 
     :param result:
     :type result: dict
     :param master_key:
     :type master_key: int
-    :param child_key:
-    :type child_key: int
     :rtype: any
     """
     if result.get(master_key):
-        return result[master_key][child_key]
+        return set_front_operand_helper(result, master_key)
     return ' '
 
 
@@ -226,56 +250,74 @@ def set_result(result: dict, master_key: int, child_key: int) -> any:
     :rtype: any
     """
     if result.get(master_key):
-        return result[master_key][child_key]
+        padding = max(len(str(result[master_key][1])), len(str(result[master_key][2])))
+        return '''{0:>{1}}'''.format(result[master_key][child_key], padding + 2)
     return ' '
 
 
-def set_operator_line(result: dict, master_key: int, front_operand_key, back_operand_key: int, operator_key: int) -> str:
+def set_back_operand_helper(result: dict, master_key: int) -> str:
+    """
+    Helper function to set_back_operand function
+    :param result:
+    :type result: dict
+    :param master_key:
+    :type master_key: int
+    :return: str
+    """
+    left_to_right_operand_mapping = {
+        4: {4: 1, 3: 2, 2: 3, 1: 4},
+        3: {3: 1, 2: 2, 1: 3},
+        2: {2: 1, 1: 2},
+        1: {1: 1}
+    }
+
+    # check which is the bigger of the two operands
+    print(f"Master key 1: {len(str(result[master_key][2]))}")
+    if len(str(result[master_key][2])) >= len(str(result[master_key][1])):
+        # return the space padded string
+        return '''{0:>{1}}'''.format(result[master_key][2], len(str(result[master_key][2])) + 1)
+    else:
+        padding = left_to_right_operand_mapping[len(str(result[master_key][1]))][len(str(result[master_key][2]))]
+        # print(f"Padding: {padding}")
+        return '''{0:>{1}}'''.format(result[master_key][2], padding + len(str(result[master_key][2])))
+
+
+def set_operator_line(result: dict, master_key: int) -> str:
     """set_operator_line.
 
     :param result:
     :type result: dict
     :param master_key:
     :type master_key: int
-    :param front_operand_key:
-    :param back_operand_key:
-    :type back_operand_key: int
-    :param operator_key:
-    :type operator_key: int
     :rtype: str
     """
-    space_multiplier_numbers = {0: 1, 1: 2, 2: 3, 3: 4}
     if result.get(master_key):
-        f_key = len(str(result[master_key][front_operand_key]))
-        b_key = len(str(result[master_key][back_operand_key]))
-        if f_key > b_key:
-            multiplier = f_key - b_key
-            return str(result[master_key][operator_key]) + ' '*space_multiplier_numbers[multiplier] + str(result[master_key][back_operand_key])
-        return str(result[master_key][operator_key]) + ' ' + str(result[master_key][back_operand_key])
+        r_operand = set_back_operand_helper(result, master_key)
+        operator = result[master_key][4]
+        return str(operator) + str(r_operand)
     return ' '
 
 
-def display_solutions_with_results(solutions: list) -> None:
+def display_solutions_with_results(solutions: list) -> str:
     """display_solutions_with_results.
 
     :param solutions:
     :type solutions: list
-    :rtype: None
+    :rtype: str
     """
     results = {}
     for i, value in enumerate(solutions):
         results[i] = {1: value[0], 2: value[1], 3: value[2], 4: value[3]}
-
     statements = [
-        f"{set_front_operand(results, 0, 1):>5}", f"{set_front_operand(results, 1, 1):>6}", f"{set_front_operand(results, 2, 1):>6}", f"{set_front_operand(results, 3, 1):>6}", f"{set_front_operand(results, 4, 1):>6}",
-        f"{set_operator_line(results, 0, 1, 2, 4):>5}", f"{set_operator_line(results, 1, 1, 2, 4):>6}", f"{set_operator_line(results, 2, 1, 2, 4):>6}", f"{set_operator_line(results, 3, 1, 2, 4):>6}", f"{set_operator_line(results, 4, 1, 2, 4):>6}",
-        f"{calculate_line(results, 0, 1, 2):>5}", f"{calculate_line(results, 1, 1, 2):>6}", f"{calculate_line(results, 2, 1, 2):>6}", f"{calculate_line(results, 3, 1, 2):>6}", f"{calculate_line(results, 4, 1, 2):>6}",
-        f"{set_result(results, 0, 3):>5}", f"{set_result(results, 1, 3):>6}", f"{set_result(results, 2, 3):>6}", f"{set_result(results, 3, 3):>6}", f"{set_result(results, 4, 3):>6}",
+        f"{set_front_operand(results, 0)}", f"{set_front_operand(results, 1)}", f"{set_front_operand(results, 2)}", f"{set_front_operand(results, 3)}", f"{set_front_operand(results, 4)}",
+        f"{set_operator_line(results, 0)}", f"{set_operator_line(results, 1)}", f"{set_operator_line(results, 2)}", f"{set_operator_line(results, 3)}", f"{set_operator_line(results, 4)}",
+        f"{calculate_line(results, 0, 1, 2):>}", f"{calculate_line(results, 1, 1, 2):>}", f"{calculate_line(results, 2, 1, 2):>}", f"{calculate_line(results, 3, 1, 2):>}", f"{calculate_line(results, 4, 1, 2):>}",
+        f"{set_result(results, 0, 3):>}", f"{set_result(results, 1, 3):>}", f"{set_result(results, 2, 3):>}", f"{set_result(results, 3, 3):>}", f"{set_result(results, 4, 3):>}",
                 ]
 
     _assist = {
         0: [0,  len(solutions)],
-        1: [5,  5  + len(solutions)],
+        1: [5,  5 + len(solutions)],
         2: [10, 10 + len(solutions)],
         3: [15, 15 + len(solutions)]
 
@@ -284,40 +326,38 @@ def display_solutions_with_results(solutions: list) -> None:
     send = ""
 
     for k in _assist:
-        #print("In assist")
+        # print("In assist")
         statements_slice = statements[_assist[k][0]:_assist[k][1]]
-        print(f"Statements: {statements_slice}")
+        # print(f"Statements: {statements_slice}")
         if [*_assist.keys()][-1] != k:
-            send += "   ".join(statements_slice)
+            send += "    ".join(statements_slice)
             send += "\n"
         else:
-            send +=  "   ".join(statements_slice)
+            send += "    ".join(statements_slice)
 
     return send
 
 
-def display_solutions_without_results(solutions: list) -> None:
+def display_solutions_without_results(solutions: list) -> str:
     """display_solutions_without_results.
 
     :param solutions:
     :type solutions: list
-    :rtype: None
+    :rtype: str
     """
     results = {}
     for i, value in enumerate(solutions):
         results[i] = {1: value[0], 2: value[1], 3: value[2], 4: value[3]}
-    print(f"Solutions: {solutions}")
     statements = [
-        f"{set_front_operand(results, 0, 1):>3}", f"{set_front_operand(results, 1, 1):>6}", f"{set_front_operand(results, 2, 1):>4}", f"{set_front_operand(results, 3, 1):>4}", f"{set_front_operand(results, 4, 1):>4}",
-        f"{set_operator_line(results, 0, 1, 2, 4):>}", f"{set_operator_line(results, 1, 1, 2, 4):>}", f"{set_operator_line(results, 2, 1, 2, 4):>}", f"{set_operator_line(results, 3, 1, 2, 4):>}", f"{set_operator_line(results, 4, 1, 2, 4):>}",
-        f"{calculate_line(results, 0, 1, 2):>}", f"{calculate_line(results, 1, 1, 2):>4}", f"{calculae_line(results, 2, 1, 2):>}", f"{calculate_line(results, 3, 1, 2):>}", f"{calculate_line(results, 4, 1, 2):>}",
+        f"{set_front_operand(results, 0)}", f"{set_front_operand(results, 1)}", f"{set_front_operand(results, 2)}", f"{set_front_operand(results, 3)}", f"{set_front_operand(results, 4)}",
+        f"{set_operator_line(results, 0)}", f"{set_operator_line(results, 1)}", f"{set_operator_line(results, 2)}", f"{set_operator_line(results, 3)}", f"{set_operator_line(results, 4)}",
+        f"{calculate_line(results, 0, 1, 2):>}", f"{calculate_line(results, 1, 1, 2):>}", f"{calculate_line(results, 2, 1, 2):>}", f"{calculate_line(results, 3, 1, 2):>}", f"{calculate_line(results, 4, 1, 2):>}",
                 ]
-    # print(f"One: {statements[0]}")
     # print(f"Statements: {statements}")
     _assist = {
-        0: [ 0, len(solutions)],
-        1: [ 5, 5 + len(solutions)],
-        2: [ 10, 10 + len(solutions)],
+        0: [0, len(solutions)],
+        1: [5, 5 + len(solutions)],
+        2: [10, 10 + len(solutions)],
 
     }
 
@@ -325,22 +365,23 @@ def display_solutions_without_results(solutions: list) -> None:
 
     for k in _assist:
         statements_slice = statements[_assist[k][0]:_assist[k][1]]
-        print(f"Statements: {statements_slice}")
+        # print(f"Statements: {statements_slice}")
         if [*_assist.keys()][-1] != k: # only add newline character if not last key
-            send += "   ".join(statements_slice)
+            send += "    ".join(statements_slice)
             send += "\n"
         else:
-            send +=  "   ".join(statements_slice)
+            send += "    ".join(statements_slice)
     return send
 
 
-
 def arithmetic_arranger(my_list: list, result: bool = False) -> any:
-    """arithmetic_arranger.
-
+    """
+    Main entry point for app
     :param my_list:
     :type my_list: list
-    :rtype: any
+    :param result:
+    :type result: bool
+    :return:
     """
     problem_count = problems_number_limit_check(my_list)
     operator_checker = operator_check(my_list)
@@ -351,10 +392,7 @@ def arithmetic_arranger(my_list: list, result: bool = False) -> any:
         if operator_checker:
             if digit_checker:
                 if digits_count_checker:
-                    try:
-                        results = extract_digits(my_list)
-                    except TypeError as e_err:
-                        print(f"Error: results error {e_err}")
+                    results = extract_digits(my_list)
                     try:
                         if result:
                             return display_solutions_with_results(results)
