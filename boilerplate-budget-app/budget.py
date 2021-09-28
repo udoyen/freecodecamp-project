4c4046b17177467445f545189832d65d57907de8
udoyen <datameshprojects@gmail.com>
2021-09-28 02:41:51 +0100

    all tests passed for budget app

--- boilerplate-budget-app/budget.py
+++ boilerplate-budget-app/budget.py
@@ -8,7 +8,7 @@ class Category:
         category_name = self.category
         current_ledger = self.ledger
         border_line_multiplier = int((30 - len(category_name)) / 2)
-        intro_line = '*'*border_line_multiplier + category_name + '*'*border_line_multiplier
+        intro_line = '*' * border_line_multiplier + category_name + '*' * border_line_multiplier
 
         ledger_list_items = '' + intro_line + "\n"
         for i in current_ledger:
@@ -104,5 +104,89 @@ class Category:
         return True
 
 
-def create_spend_chart(categories):
-    pass
+def split(word):
+    """
+    Helper function to
+    split catergory words
+    :param word:
+    :type: str
+    :return:
+    :rtype: list
+    """
+    return [char for char in word]
+
+
+def create_spend_chart(categories: list) -> str:
+    """
+    Function to create chart
+    :param categories:
+    :type: list
+    :return:
+    :rtype: str
+    """
+    cat_items = []
+    total_percentage_spend = 0
+    for i in categories:  # list of categories
+        init_balance = 0
+        balance = i.get_balance()
+        for j in i.ledger:  # list of dicts
+            if init_balance != 0:
+                break
+            for key in j:
+                if init_balance != 0:
+                    break
+                if key == 'description':
+                    init_balance = j['amount']
+                    calculate_percentage = int(((init_balance - balance) * 100) / init_balance)
+                    total_percentage_spend += calculate_percentage
+                    cat_items.append({i.category: calculate_percentage})
+                    break
+    # Calculate the display percentage
+    for k in cat_items:
+        for key, value in k.items():
+            k[key] = int(((value * 100) / total_percentage_spend)) - (
+                        int(((value * 100) / total_percentage_spend)) % 10)
+    # print(f"Items: {cat_items}")
+    header = "Percentage spent by category" + "\n"
+    display = ''
+    display += header
+    for i in range(100, -1, -10):
+        display += f"{i:>3}| "
+        for index, value in enumerate(cat_items):
+            if index == 0:
+                for j in value.values():
+                    if j >= i:
+                        display += f"o  "
+                    else:
+                        display += "   "
+            else:
+                for j in value.values():
+                    if j >= i:
+                        display += f"o  "
+                    else:
+                        display += f"   "
+
+        display += "\n"
+    display += "    " + "-" * 10 + "\n"
+    # display += "     "
+    total_letter_count = 0
+    combined_keys_list = []
+    for item in cat_items:
+        for k in item.keys():
+            total_letter_count += len(k)
+            combined_keys_list.append(split(k))
+    # print(total_letter_count, combined_keys_list)
+    for c in range(0, total_letter_count):
+        for index, m in enumerate(combined_keys_list):
+            if index == 0:
+                if len(m) != 0:
+                    display += "     " + m.pop(0) + "  "
+                else:
+                    display += "        "
+            else:
+                if len(m) != 0:
+                    display += m.pop(0) + "  "
+                else:
+                    display += "   "
+        display += "\n"
+    return display.rstrip() + "  "
